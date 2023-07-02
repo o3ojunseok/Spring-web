a436c4d71125c4b3b17e6e96bc92181b2405131c
o3ojunseok <o3ojunseok@gmail.com>
2023-07-02 10:00:00 +0900

    test

--- springboot/src/test/java/com/jun/springboot/web/HelloControllerTest.java
+++ springboot/src/test/java/com/jun/springboot/web/HelloControllerTest.java
@@ -12,18 +12,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class) // RunWith -> 스프링부트 테스트랑 Junit 연결역할
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class) // SpringMvc에 집중, 선언하면 @Controller, @ControllerAdvice등 사용가능, @Service,@Repository는 불가능, 컨트롤러 전용
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired // 스프링이 관리하는 빈 주입 받기
+    private MockMvc mvc; // MVC테스트 시작점, API테스트 가능
 
     @Test
     public void helloReturn() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        mvc.perform(get("/hello")) // 요청함, 체이닝이 지원돼 여러 검증기능 이어서 가능
+                .andExpect(status().isOk()) // status검증
+                .andExpect(content().string(hello)); // 응답내용 검증
     }
 }
